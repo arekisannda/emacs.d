ec0aa28fbc357fc49de6f890ad760600f8e724f7
Alex <alex@arekisannda.dev>
2023-10-03 01:50:56 -0700

    refactor + update keybindings

--- keybinds/init.el
+++ keybinds/init.el
@@ -6,38 +6,58 @@
 ;;; code:
 
 (general-define-key
- (kbd "<escape>") #'keyboard-escape-quit
- (kbd "C-q")      #'keyboard-quit
+ (kbd "<escape>")  #'keyboard-escape-quit
+ (kbd "C-q")       #'keyboard-quit
 
  (kbd "C-<left>")  #'evil-window-left
  (kbd "C-<right>") #'evil-window-right
  (kbd "C-<up>")    #'evil-window-up
- (kbd "C-<down>")  #'evil-window-down)
+ (kbd "C-<down>")  #'evil-window-down
+
+ (kbd "S-<left>") (lambda () (interactive)(evil-scroll-column-left 5))
+ (kbd "S-<right>") (lambda () (interactive)(evil-scroll-column-right 5))
+ (kbd "S-<up>") (lambda () (interactive)(evil-scroll-line-up 10))
+ (kbd "S-<down>") (lambda () (interactive)(evil-scroll-line-down 10)))
 
 ;;;;
 ;; general keybinds
 ;;;;
 (general-define-key
- "C-\\" #'vterm
- "C-|"  #'multi-vterm
+ "C-\\" #'popper-toggle
+ "C-|"  #'popper-toggle-type
+
  "M-["  #'tab-bar-switch-to-prev-tab
  "M-]"  #'tab-bar-switch-to-next-tab
- "C-,"  #'company-complete
- "C-."  #'company-show-doc-buffer)
+
+ "C-," #'company-capf
+ "C-." #'company-show-doc-buffer)
 
 ;; vterm keybinds
-(general-def vterm-mode-map
-  "C-\\" #'switch-to-prev-buffer
-  "M-]"  #'tab-bar-switch-to-next-tab
+(general-define-key
+ :keymaps '(vterm-mode-map)
+ "C-\\" #'popper-toggle
+ "M-]"  #'tab-bar-switch-to-next-tab
 
-  (kbd "C-<left>")  #'evil-window-left
-  (kbd "C-<right>") #'evil-window-right
-  (kbd "C-<up>")    #'evil-window-up
-  (kbd "C-<down>")  #'evil-window-down)
+ (kbd "C-<left>")  #'evil-window-left
+ (kbd "C-<right>") #'evil-window-right
+ (kbd "C-<up>")    #'evil-window-up
+ (kbd "C-<down>")  #'evil-window-down
 
+ (kbd "S-<left>") (lambda () (interactive)(evil-scroll-column-left 5))
+ (kbd "S-<right>") (lambda () (interactive)(evil-scroll-column-right 5))
+ (kbd "S-<up>") (lambda () (interactive)(evil-scroll-line-up 10))
+ (kbd "S-<down>") (lambda () (interactive)(evil-scroll-line-down 10)))
 
-(general-def minibuffer-local-map
-  "M-A" #'marginalia-cycle)
+(general-define-key
+ :keymaps '(minibuffer-local-map)
+ "M-A" #'marginalia-cycle
+ "M-s" #'consult-history
+ "M-r" #'consult-history)
+
+(general-define-key
+ :keymaps '(popper-mode-map)
+ (kbd "M-<prior>") #'popper-cycle-backwards
+ (kbd "M-<next>")  #'popper-cycle)
 
 ;;;;
 ;; vim-mode keybinds
@@ -61,107 +81,86 @@
   :global-prefix "M-;")
 
 ;; bind evil-args text objects
-(general-def evil-inner-text-objects-map
-  "a" #'evil-inner-arg)
+(general-define-key
+ :keymaps '(evil-inner-text-objects-map)
+ "a" #'evil-inner-arg)
 
-(general-def evil-outer-text-objects-map
-  "a" #'evil-outer-arg)
+(general-define-key
+ :keymaps '(evil-outer-text-objects-map)
+ "a" #'evil-outer-arg)
 
-(general-def evil-window-map
-  "V" #'split-window-horizontally
-  "v" (lambda () (interactive)(split-window-horizontally)(other-window 1))
-  "S" #'split-window-vertically
-  "s" (lambda () (interactive)(split-window-vertically)(other-window 1)))
+(general-define-key
+ :keymaps '(evil-window-map)
+ "V" #'split-window-horizontally
+ "v" (lambda () (interactive)(split-window-horizontally)(other-window 1))
+ "S" #'split-window-vertically
+ "s" (lambda () (interactive)(split-window-vertically)(other-window 1)))
 
-(general-def evil-normal-state-map
-  ;; bind evil-forward/backward-args
-  "L"  #'evil-forward-arg
-  "H"  #'evil-backwards-arg
-  "K"  #'evil-jump-out-args
-  "u"  #'undo-fu-only-undo
-  "U"  #'undo-fu-only-redo
+(general-define-key
+ :keymaps '(evil-normal-state-map)
+ ;; bind evil-forward/backward-args
+ "L"  #'evil-forward-arg
+ "H"  #'evil-backwards-arg
+ "K"  #'evil-jump-out-args
+ "u"  #'undo-fu-only-undo
+ "U"  #'undo-fu-only-redo
 
-  "]g" #'diff-hl-next-hunk
-  "[g" #'diff-hl-previous-hunk)
+ "]g" #'diff-hl-next-hunk
+ "[g" #'diff-hl-previous-hunk)
 
-(general-def evil-motion-state-map
-  ;; bind evil-forward/backward-args
-  "L" #'evil-forward-arg
-  "H" #'evil-backwards-arg)
+(general-define-key
+ :keymaps '(evil-motion-state-map)
+ ;; bind evil-forward/backward-args
+ "L" #'evil-forward-arg
+ "H" #'evil-backwards-arg)
 
-;; utility/tool keybinds
 (kb/leader-key
+  "M-\\" '(vterm :wk "open vterm")
+  "M-|"  '(multi-vterm :wk "new multi-vterm")
+
+  ;; terminal keybinds
+  "vr" '(multi-vterm-rename-buffer :wk "rename terminal")
+
+  ;; utility/tool keybinds
   "p"  '(:ignore t :wk "tools")
+  "pe" '(eval-region :wk "eval-region")
   "pE" '(eval-buffer :wk "eval-buffer")
   "pp" '(elpaca-try :wk "elpaca-try")
   "pP" '(elpaca-manager :wk "elpaca-manager")
   "pf" '(project-switch-project :wk "projects")
   "pF" '(ranger :wk "ranger")
   "ps" '(scratch-buffer :wk "scratch buffer")
-  "pt" '(popper-toggle-latest :wk "toggle window")
-  "pT" '(popper-toggle-type :wk "toggle window type"))
+  "pk" '((lambda () (interactive)(project-kill-buffers)(delete-other-windows)(dashboard-open)) :wk "kill project buffers")
 
-;; quick window switcher keybinds
-(kb/leader-key
-  "s"  '(:ignore t :wk "switcher")
-  "ss" '(ace-window :wk "select")
-  "sS" '(ace-swap-window :wk "swap")
-  "sd" '(ace-delete-window :wk "delete"))
-
-;; tab management keybinds
-(kb/leader-key
-  "t"  '(:ignore t :wk "tabs")
-  "tt" '((lambda () (interactive)(tab-bar-new-tab)(dashboard-open)) :wk "new tab")
-  "tk" '(tab-bar-close-tab :wk "kill tab")
-  "tK" '(tab-bar-close-other-tabs :wk "kill other tabs")
-  "tr" '(tab-bar-rename-tab :wk "rename tab"))
-
-;; buffer management keybinds
-(kb/leader-key
+  ;; buffer management keybinds
   "b"  '(:ignore t :wk "buffer")
   "bb" '(consult-project-buffer :wk "switch buffer")
   "bB" '(consult-buffer :wk "switch buffer (all)")
   "bX" '(kill-buffer :wk "kill buffer")
   "bk" '(kill-this-buffer :wk "kill current buffer")
-  "bK" '(project-kill-buffers :wk "kill project buffers")
+  "bK" '(project-kill-buffers :wk "kill current buffer")
   "bn" '(next-buffer :wk "next buffer")
   "bp" '(previous-buffer :wk "previous buffer")
-  "br" '(revert-buffer :wk "reload buffer"))
+  "br" '(revert-buffer :wk "reload buffer")
 
-;; mode management keybinds
-(kb/leader-key
+  ;; quick window switcher keybinds
+  "s"  '(:ignore t :wk "switcher")
+  "ss" '(ace-window :wk "select")
+  "sS" '(ace-swap-window :wk "swap")
+  "sd" '(ace-delete-window :wk "delete")
+
+  ;; tab management keybinds
+  "t"  '(:ignore t :wk "tabs")
+  "tt" '((lambda () (interactive)(tab-bar-new-tab)(dashboard-open)) :wk "new tab")
+  "tk" '(tab-bar-close-tab :wk "kill tab")
+  "to" '(tab-bar-close-other-tabs :wk "kill other tabs")
+  "tr" '(tab-bar-rename-tab :wk "rename tab")
+
+  ;; mode management keybinds
   "m"  '(:ignore t :wk "modes")
+  "mI" '(image-mode :wk "image mode")
+  "mf" '(fundamental-mode :wk "fundamental mode")
   "mr" '(rainbow-mode :wk "rainbow mode"))
-
-;; editor keybinds
-(kb/local-leader-key
-  ","  '(:ignore t :wk "editor")
-  ",=" '(sort-lines :wk "sort lines")
-  ",k" '(align :wk "basic align")
-  ",l" '(evil-lion-left :wk "align left")
-  ",L" '(evil-lion-right :wk "align righ")
-  ",c" '(evilnc-comment-or-uncomment-lines :wk "comment-line")
-  ",C" '(evilnc-comment-or-uncomment-paragraphs :wk "comment-paragraph")
-  ",s" '(embrace-add :wk "surround add")
-  ",S" '(embrace-change :wk "surround change")
-  ",d" '(embrace-delete :wk "surround delete"))
-
-;; lsp
-(kb/local-leader-key
-  "d" '(:ignore t :wk "lsp")
-  "dr" '(lsp-rename :wk "rename"))
-
-;; editor motion keybinds
-(kb/local-leader-key
-  "m"  '(:ignore t :wk "motion")
-  "mj" '(evilem-motion-find-char :wk "fowards motion")
-  "mk" '(evilem-motion-find-char-backward :wk "backwards motion")
-  "mJ" '(evilem-motion-find-char-to :wk "forwards motion to")
-  "mK" '(evilem-motion-find-char-to-backward :wk "backwards motion to")
-  "ms" '(evil-snipe-s :wk "forwards inclusive snipe")
-  "mS" '(evil-snipe-S :wk "backwards inclusive snipe")
-  "mx" '(evil-snipe-x :wk "forwards exclusive snipe")
-  "mX" '(evil-snipe-X :wk "backwards exclusive snipe"))
 
 (setq diff-hl-show-hunk--current-footer
       "(q)Quit  (j)Next  (k)Previous  (s)Stage  (d)Revert  (c)Copy original")
@@ -175,8 +174,40 @@
   ("c" #'diff-hl-show-hunk-copy-original-text :hint nil))
 (hydra-set-property 'hydra-git-hunk :verbosity 0)
 
-;; git keybinds
 (kb/local-leader-key
+  ;; editor keybinds
+  ","  '(:ignore t :wk "editor")
+  ",=" '(sort-lines :wk "sort lines")
+  ",k" '(align :wk "basic align")
+  ",l" '(evil-lion-left :wk "align left")
+  ",L" '(evil-lion-right :wk "align righ")
+  ",c" '(evilnc-comment-or-uncomment-lines :wk "comment-line")
+  ",C" '(evilnc-comment-or-uncomment-paragraphs :wk "comment-paragraph")
+  ",s" '(embrace-add :wk "surround add")
+  ",S" '(embrace-change :wk "surround change")
+  ",d" '(embrace-delete :wk "surround delete")
+  ",r" '(lsp-rename :wk "rename symbol")
+
+  ;; lsp
+  "d"  '(:ignore t :wk "lsp")
+  "di" '(lsp-describe-thing-at-point :wk "find definition")
+  "dI" '(lsp-find-implementation :wk "find implementation")
+  "dt" '(lsp-find-type-definition :wk "find type definition")
+  "dd" '(lsp-find-definition :wk "find definition")
+  "dr" '(lsp-find-references :wk "find reference")
+
+  ;; editor motion keybinds
+  "m"  '(:ignore t :wk "motion")
+  "mj" '(evilem-motion-find-char :wk "fowards motion")
+  "mk" '(evilem-motion-find-char-backward :wk "backwards motion")
+  "mJ" '(evilem-motion-find-char-to :wk "forwards motion to")
+  "mK" '(evilem-motion-find-char-to-backward :wk "backwards motion to")
+  "ms" '(evil-snipe-s :wk "forwards inclusive snipe")
+  "mS" '(evil-snipe-S :wk "backwards inclusive snipe")
+  "mx" '(evil-snipe-x :wk "forwards exclusive snipe")
+  "mX" '(evil-snipe-X :wk "backwards exclusive snipe")
+
+  ;; git keybinds
   "g"  '(:ignore t :wk "git")
   "gi" '(hydra-git-hunk/diff-hl-show-hunk :wk "inspect")
   "gj" '(diff-hl-next-hunk :wk "next hunk")
@@ -184,10 +215,8 @@
   "gs" '(diff-hl-stage-current-hunk :wk "stage")
   "gd" '(diff-hl-revert-hunk :wk "revert"))
 
-;;;;
-;; consult keybinds
-;;;;
 (kb/search-leader-key
+  ;; consult keybinds
   "g" #'consult-ripgrep
   "r" #'consult-recent-file
   "f" #'project-find-file
@@ -197,10 +226,6 @@
   "k" #'consult-kmacro
   "m" #'consult-man
   "i" #'consult-info)
-
-(general-def minibuffer-local-map
-  "M-s" #'consult-history
-  "M-r" #'consult-history)
 
 (provide 'keybinds-init)
 ;;; init.el ends here
