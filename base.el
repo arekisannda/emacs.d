d4af1e3b6cbbed0b79f5227c39db136db50b37bd
Alex <alex@arekisannda.dev>
2023-11-17 12:36:52 -0800

    add warning log level

--- base.el
+++ base.el
@@ -20,6 +20,8 @@
 (visual-line-mode -1)
 (electric-pair-mode 1)
 
+;; (setq-default warning-minimum-level :emergency)
+
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 
 ;; theme configurations
