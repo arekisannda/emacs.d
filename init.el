9cf67e040a98b49151bdddb4e13dac38c8ab1ae8
Alex <alex@arekisannda.dev>
2023-11-20 23:13:26 -0800

    update elpaca manager

--- init.el
+++ init.el
@@ -3,14 +3,19 @@
 
 ;;; Code:
 
+;; set directories
+(defvar user-configs-directory "~/.config")
+(defvar user-emacs-directory "~/.config/emacs")
+(defvar user-code-directory "~/Code")
+
 ;; setup elpaca package manager
-(defvar elpaca-installer-version 0.5)
+(defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil
-                              :files (:defaults (:exclude "extensions"))
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
@@ -46,30 +51,33 @@
   ;; Enable :elpaca use-package keyword.
   (elpaca-use-package-mode)
   ;; Assume :elpaca t unless otherwise specified.
-  (setq elpaca-use-package-by-default t))
-(setq elpaca-hide-initial-build t)
-(setq elpaca-hide-status-during-build t)
+  (setq elpaca-use-package-by-default t)
+  (setq elpaca-hide-initial-build t)
+  (setq elpaca-hide-status-during-build t))
+
 
 ;; Block until current queue processed.
 (elpaca-wait)
-
-;; set directories
-(defvar user-configs-directory "~/.config")
-(defvar user-emacs-directory "~/.config/emacs")
-(defvar user-code-directory "~/Code")
 
 (load-file (expand-file-name "utils/init.el" user-emacs-directory))
 (load-file (expand-file-name "configs/init.el" user-emacs-directory))
 (load-file (expand-file-name "packages/init.el" user-emacs-directory))
 (load-file (expand-file-name "keybinds/init.el" user-emacs-directory))
 
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
+(setq-default confirm-nonexistent-file-or-buffer nil)
+(setq-default inhibit-startup-screen t)
+(setq-default confirm-kill-processes nil)
+(setq-default auto-save-default nil)
+(setq-default make-backup-files nil)
+(setq-default create-lockfiles nil)
+
 ;; (setq project-vc-merge-submodules t)
 (setq project-vc-extra-root-markers '(".project.el" ".projectile" ".project"))
 (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(setq-default message-log-max 2000)
+(kill-buffer "*Messages*")
 (provide 'config-init)
 
 ;;; init.el ends here
