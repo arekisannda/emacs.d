7ad9b5f565f7c3eaf1123049d1d747d3e82cc8cc
Alex <alex@arekisannda.dev>
2024-01-24 02:55:21 -0800

    add headers to custom.el

--- custom.el
+++ custom.el
@@ -1,3 +1,8 @@
+;;; custom.el --- Emacs custom configurations -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -11,3 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'custom)
+
+;;; custom.el ends here
