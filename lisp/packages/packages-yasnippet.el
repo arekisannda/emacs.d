c3fde1e5ae26ef9829712e37bb0326ee9ac10ffd
Alex <alex@arekisannda.dev>
2024-04-04 16:57:50 -0700

    update yasnippet configurations to be immediately loaded

--- lisp/packages/packages-yasnippet.el
+++ lisp/packages/packages-yasnippet.el
@@ -7,18 +7,12 @@
   :hook
   (elpaca-after-init . yas-global-mode))
 
-(use-package yasnippet-snippets :after yasnippet)
-
-(use-package emacs
-  :ensure nil
-  :preface
-  (defun +yasnippet-add-dirs ()
-    (let ((dir (expand-file-name "emacs-snippets" +user-config-dir)))
-      (unless (member dir yas-snippet-dirs)
-        (add-to-list 'yas-snippet-dirs dir)
-        (yas--load-snippet-dirs))))
-  :hook
-  (elpaca-after-init . +yasnippet-add-dirs))
+(use-package yasnippet-snippets :after yasnippet
+  :config
+  (let ((dir (expand-file-name "emacs-snippets" +user-config-dir)))
+    (unless (member dir yas-snippet-dirs)
+      (add-to-list 'yas-snippet-dirs dir)
+      (yas--load-snippet-dirs))))
 
 (provide 'packages-yasnippet)
 
