b57d73da5e6bd6eb90aa3d9d3c35312a5c19cb22
Alex <alex@arekisannda.dev>
2024-05-03 01:07:06 -0700

    update whitespace configurations

--- lisp/lang/lang-whitespace.el
+++ lisp/lang/lang-whitespace.el
@@ -10,7 +10,8 @@
   (go-ts-mode-indent-offset 4)
   (json-ts-mode-indent-offset 2)
   (yaml-indent-offset 2)
-  (typst-ts-mode-indent-offset 2))
+  (typst-ts-mode-indent-offset 2)
+  (sh-basic-offset 2))
 
 (provide 'lang-whitespace)
 
