574d28e2fbbda82897e90c9f8818a4682db9fa33
Alex <alex@arekisannda.dev>
2026-03-07 18:01:38 -0800

    update script configurations

--- lisp/scripts/swaywm.el
+++ lisp/scripts/swaywm.el
@@ -13,7 +13,8 @@
 (defun swaywm/notes-open (title)
   (util/frames-select-frame-with-params `((+side-frame . t)
                                           (name . ,notes-frame-name))
-    (org-roam-node-visit (org-roam-node-from-title-or-alias title t))))
+    (org-roam-node-visit (org-roam-node-from-title-or-alias title t))
+    (tab-line-close-other-tabs)))
 
 ;;;###autoload
 (defun swaywm/notes-create (key title)
@@ -22,4 +23,5 @@
     (let ((buffer (generate-new-buffer "*new*")))
       (set-buffer-major-mode buffer)
       (set-window-buffer nil buffer))
-    (org-roam-capture- :keys key :node (org-roam-node-create :title title))))
+    (org-roam-capture- :keys key :node (org-roam-node-create :title title))
+    (tab-line-close-other-tabs)))
